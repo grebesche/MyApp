d9ad4607e6bdf4e8916ecfc1f5c45e2d6b308397
grebesche <guillaume.rebesche@gmail.com>
2015-08-28 19:00:59 -0400

    add the eureka discovery service and tried to connect clients to it (failed for now)

--- events-service/src/main/java/com/guillaume/bees/events/EventsServiceApplication.java
+++ events-service/src/main/java/com/guillaume/bees/events/EventsServiceApplication.java
@@ -2,8 +2,11 @@ package com.guillaume.bees.events;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class EventsServiceApplication {
 
     public static void main(String[] args) {
