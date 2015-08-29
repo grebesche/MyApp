c58e81e5248cc950bb33e62315457b32db58ea97
grebesche <guillaume.rebesche@gmail.com>
2015-08-29 02:09:08 -0400

    fix api artifact name

--- events-service/src/main/java/com/guillaume/bees/events/EventsService.java
+++ events-service/src/main/java/com/guillaume/bees/events/EventsService.java
@@ -3,9 +3,6 @@ package com.guillaume.bees.events;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by guillaume on 25/08/15
- */
 @RestController
 public class EventsService {
 
