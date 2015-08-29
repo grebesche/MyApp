414d13de16a2fe4bf9ce0b12e215779742d9f1de
grebesche <guillaume.rebesche@gmail.com>
2015-08-29 02:31:37 -0400

    add the emails service skeleton

---
+++ emails-service/src/main/java/com/guillaume/bees/emails/EmailsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.guillaume.bees.emails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailsServiceApplication.class, args);
+    }
+}
